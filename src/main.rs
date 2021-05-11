3be85b42549056d309573136fc6f59cf5586d334
Akshith Gunasekaran <akshith.573@gmail.com>
2021-05-10 18:47:50 -0700

    adds cli using structopt - list - add task:string - edit id:i32 task:string - done id:i32

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Cli {
+    List {
+    },
+    Add {
+        #[structopt()]
+        task: String,
+    },
+    Edit {
+        #[structopt()]
+        id: i32,
+        #[structopt()]
+        task: String,
+    },
+    Done {
+        #[structopt()]
+        id: i32,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    // command line arguments
+    match Cli::from_args() {
+        Cli::Add { task } => {
+            println!("New task: {}", task)
+        },
+        Cli::Edit { id, task } => {
+            println!("Edit task: {} {}", id, task)
+        },
+        Cli::Done { id } => {
+            println!("Done task: {}", id)
+        },
+        Cli::List {} => {
+            println!("list of tasks")
+        },
+    }
 }
